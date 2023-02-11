a79fc6924dcea905b715d36cba346c2e5eb2233d
Mates <Mates@DESKTOP-HAP8GPA>
2023-02-11 15:19:02 +0100

    Eddited entity Policyholder

--- AspProjektPojisteni/Models/Policyholder.cs
+++ AspProjektPojisteni/Models/Policyholder.cs
@@ -11,25 +11,40 @@ namespace AspProjektPojisteni.Models
 
         [Display(Name = "Číslo pojištěnce")]
         public int ID { get; set; }
+
+        [Display(Name = "Titul před")]
+        public string DegreeBefore { get; set; }
+        
         [Required(ErrorMessage = "Poviný údaj")]
         [Display(Name = "Křestní jméno")]
         public string FirstName { get; set; }
+        
         [Required(ErrorMessage = "Poviný údaj")]
         [Display(Name = "Příjmení")]
         public string LastName { get; set; }
+
+        [Display(Name = " Titul za")]
+        public string DegreeAfter { get; set; }
+
+        public string FullName { get; set; }
+
         [Required]
         [EmailAddress(ErrorMessage = "Neplatná emailová adresa")]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Poviný údaj")]
         [Display(Name = "Telefoní číslo")]
         public string PhoneNumber { get; set; }
+
         [Required(ErrorMessage = "Poviný údaj")]
         [Display(Name = "Ulice a číslo popisné")]
         public string Address { get; set; }
+
         [Required(ErrorMessage = "Poviný údaj")]
         [Display(Name = "Město")]
         public string City { get; set; }
+
         [Required(ErrorMessage = "Poviný údaj")]
         [Display(Name = "PSČ")]
         public string ZIP { get; set; }
