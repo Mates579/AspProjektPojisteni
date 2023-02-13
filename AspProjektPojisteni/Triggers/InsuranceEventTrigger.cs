3c6a97f01dd0c46975bd0ca71c0cb6e6ac5ce1ee
Mates <Mates@DESKTOP-HAP8GPA>
2023-02-13 21:20:20 +0100

    Added Policyholder & InsuranceEvent Triggers

---
+++ AspProjektPojisteni/Triggers/InsuranceEventTrigger.cs
@@ -0,0 +1,27 @@
+﻿using AspProjektPojisteni.Data;
+using AspProjektPojisteni.Models;
+using EntityFrameworkCore.Triggered;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspProjektPojisteni.Triggers
+{
+    public class InsuranceEventTrigger : IBeforeSaveTrigger<InsuranceEvent>
+    {
+        private readonly ApplicationDbContext db;
+
+        public async Task BeforeSave(ITriggerContext<InsuranceEvent> context, CancellationToken cancellationToken)
+        {
+            if (context.ChangeType == ChangeType.Added || context.ChangeType == ChangeType.Modified)
+            {
+                InsuranceEvent insuranceEvent = context.Entity;
+
+                Insurance insurance = await db.Insurance.FirstOrDefaultAsync(i => i.ID == insuranceEvent.InsuranceID);
+
+                if (insuranceEvent.Payout! > insurance.InsuranceRate)
+                {
+                    await db.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
---
+++ AspProjektPojisteni/Triggers/PolicyholderTrigger.cs
@@ -0,0 +1,31 @@
+﻿using AspProjektPojisteni.Data;
+using AspProjektPojisteni.Models;
+using EntityFrameworkCore.Triggered;
+using System.Text;
+
+namespace AspProjektPojisteni.Triggers
+{
+    public class PolicyholderTrigger : IBeforeSaveTrigger<Policyholder>
+    {
+        private readonly ApplicationDbContext db;
+        public Task BeforeSave(ITriggerContext<Policyholder> context, CancellationToken cancellationToken)
+        {
+            if (context.ChangeType == ChangeType.Added || context.ChangeType == ChangeType.Modified)
+            {
+                Policyholder policyholder = context.Entity;
+
+                policyholder.FullName = new StringBuilder()
+                    .Append(policyholder.DegreeBefore)
+                    .Append(" ")
+                    .Append(policyholder.FirstName)
+                    .Append(" ")
+                    .Append(policyholder.LastName)
+                    .Append(" ")
+                    .Append(policyholder.DegreeAfter)
+                    .ToString()
+                    .Trim();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
